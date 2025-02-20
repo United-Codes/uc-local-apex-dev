218b95aa81a8ee855e40235a6bb58731b7be09f1
phartenfeller <pharti@hotmail.de>
2025-02-20 10:53:17 +0100

    apply linting isssues

--- scripts/clear-schema.sh
+++ scripts/clear-schema.sh
@@ -11,19 +11,19 @@ if [ -z "$1" ]; then
 fi
 USERNAME=$1
 
-user_in_env $USERNAME
+user_in_env "$USERNAME"
 
-USERNAME_UPPER=$(echo $USERNAME | tr '[:lower:]' '[:upper:]')
-USERNAME_LOWER=$(echo $USERNAME | tr '[:upper:]' '[:lower:]')
+USERNAME_UPPER=$(echo "$USERNAME" | tr '[:lower:]' '[:upper:]')
+USERNAME_LOWER=$(echo "$USERNAME" | tr '[:upper:]' '[:lower:]')
 
-read -p "Dropping schema $USERNAME_UPPER. Do you want to continue? (y/n) " answer
+read -r -p "Dropping all objects in schema $USERNAME_UPPER. Do you want to continue? (y/n) " answer
 
 if [[ $answer == "y" ]] || [[ $answer == "Y" ]]; then
   echo "Continuing..."
 
   USER_DB_CONN_NAME="${DB_CONN_BASE}-${USERNAME_LOWER}"
 
-  sql -name $USER_DB_CONN_NAME <<SQL
+  sql -name "$USER_DB_CONN_NAME" <<SQL
     select user from dual;
 
     @./scripts/sql/drop_all.sql
