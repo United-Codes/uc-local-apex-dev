eb1c375fee4d731393f3e6237d8f799b3ae853d1
phartenfeller <pharti@hotmail.de>
2025-05-19 14:46:56 +0200

    Improved backups scripts and remove ESQL_ tables after dp export

--- scripts/backup-all.sh
+++ scripts/backup-all.sh
@@ -7,5 +7,7 @@ read -a db_users <<<"$(get_user_names)"
 
 for user in "${db_users[@]}"; do
   echo "Backing up $user"
-  ./scripts/backup-user.sh $user
+  ./scripts/backup-user.sh "$user" || true
 done
+
+./scripts/sync-backups-folder.sh
--- scripts/backup-user.sh
+++ scripts/backup-user.sh
@@ -11,12 +11,14 @@ if [ -z "$1" ]; then
 fi
 USERNAME=$1
 
-user_in_env $USERNAME
+user_in_env "$USERNAME"
 
-USERNAME_UPPER=$(echo $USERNAME | tr '[:lower:]' '[:upper:]')
-USERNAME_LOWER=$(echo $USERNAME | tr '[:upper:]' '[:lower:]')
+USERNAME_UPPER=$(echo "$USERNAME" | tr '[:lower:]' '[:upper:]')
+USERNAME_LOWER=$(echo "$USERNAME" | tr '[:upper:]' '[:lower:]')
 
 USER_DB_CONN_NAME="${DB_CONN_BASE}-${USERNAME_LOWER}"
+
+ORIGINAL_PWD="$PWD"
 
 # remove ./backups/$USERNAME_LOWER.log if it exists
 if [ -f ./backups/export/${USERNAME_LOWER}.log ]; then
@@ -31,7 +33,7 @@ if [ -f ./backups/export/${USERNAME_LOWER}.dmp ]; then
   mv ./backups/export/${USERNAME_LOWER}.dmp ./backups/export/${USERNAME_LOWER}_bkp.dmp
 fi
 
-sql -name $USER_DB_CONN_NAME <<SQL
+sql -name "$USER_DB_CONN_NAME" <<SQL
     select user from dual;
 
     datapump export -
@@ -57,22 +59,22 @@ SQL
 ./scripts/sync-backups-folder.sh
 
 # create directory for APEX export
-if [ ! -d ./backups/export/apex/$USERNAME_LOWER ]; then
-  mkdir -p ./backups/export/apex/$USERNAME_LOWER
+if [ ! -d ./backups/export/apex/"$USERNAME_LOWER" ]; then
+  mkdir -p ./backups/export/apex/"$USERNAME_LOWER"
 else
   # move existing files to backup folder
-  if [ ! -d ./backups/export/apex/bkp/$USERNAME_LOWER ]; then
-    mkdir -p ./backups/export/apex/bkp/$USERNAME_LOWER
+  if [ ! -d ./backups/export/apex/bkp/"$USERNAME_LOWER" ]; then
+    mkdir -p ./backups/export/apex/bkp/"$USERNAME_LOWER"
   else
     # remove existing files
-    rm -rf ./backups/export/apex/bkp/$USERNAME_LOWER/* || true
+    rm -rf ./backups/export/apex/bkp/"$USERNAME_LOWER"/* || true
   fi
-  mv ./backups/export/apex/$USERNAME_LOWER/* ./backups/export/apex/bkp/$USERNAME_LOWER
+  mv ./backups/export/apex/"$USERNAME_LOWER"/* ./backups/export/apex/bkp/"$USERNAME_LOWER"
 fi
 
-cd ./backups/export/apex/$USERNAME_LOWER
+cd ./backups/export/apex/"$USERNAME_LOWER"
 
-sql -name $USER_DB_CONN_NAME <<SQL
+sql -name "$USER_DB_CONN_NAME" <<SQL
     select user from dual;
 
     column workspace_id new_value workspace_id
@@ -80,6 +82,18 @@ sql -name $USER_DB_CONN_NAME <<SQL
 
     apex export-workspace -woi &workspace_id -overwrite-files
     apex export-all-applications -woi &workspace_id -overwrite-files
+
+    exit;
+SQL
+
+cd "$ORIGINAL_PWD"
+
+sql -name "$USER_DB_CONN_NAME" <<SQL
+    select user from dual;
+
+    set serveroutput on size unlimited
+
+    @./scripts/sql/drop_dp_tables.sql
 
     exit;
 SQL
