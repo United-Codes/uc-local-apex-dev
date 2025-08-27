a9868f268a24b8bc4b105d810211b1de2ee41729
phartenfeller <pharti@hotmail.de>
2025-08-27 16:38:56 +0200

    Comment dbms_cloud installation back in

--- scripts/install-dbms-cloud.sh
+++ scripts/install-dbms-cloud.sh
@@ -4,27 +4,25 @@ set -e
 
 source ./scripts/util/load_env.sh
 
+docker exec "$CONTAINER_NAME" bash -c "
+\$ORACLE_HOME/perl/bin/perl \$ORACLE_HOME/rdbms/admin/catcon.pl \
+  -u sys/$ORACLE_PASSWORD \
+  --force_pdb_mode 'READ WRITE' \
+  -b dbms_cloud_install \
+  -d \$ORACLE_HOME/rdbms/admin/ \
+  -l /tmp \
+  catclouduser.sql
 
+\$ORACLE_HOME/perl/bin/perl \$ORACLE_HOME/rdbms/admin/catcon.pl \
+  -u sys/$ORACLE_PASSWORD \
+  --force_pdb_mode 'READ WRITE' \
+  -b dbms_cloud_install \
+  -d \$ORACLE_HOME/rdbms/admin/ \
+  -l /tmp \
+  dbms_cloud_install.sql
+"
 
-# docker exec "$CONTAINER_NAME" bash -c "
-# \$ORACLE_HOME/perl/bin/perl \$ORACLE_HOME/rdbms/admin/catcon.pl \
-#   -u sys/$ORACLE_PASSWORD \
-#   --force_pdb_mode 'READ WRITE' \
-#   -b dbms_cloud_install \
-#   -d \$ORACLE_HOME/rdbms/admin/ \
-#   -l /tmp \
-#   catclouduser.sql
-
-# \$ORACLE_HOME/perl/bin/perl \$ORACLE_HOME/rdbms/admin/catcon.pl \
-#   -u sys/$ORACLE_PASSWORD \
-#   --force_pdb_mode 'READ WRITE' \
-#   -b dbms_cloud_install \
-#   -d \$ORACLE_HOME/rdbms/admin/ \
-#   -l /tmp \
-#   dbms_cloud_install.sql
-# "
-
-# echo "DBMS Cloud installation completed."
+echo "DBMS Cloud installation completed."
 
 
 TEMP_DIR=$(mktemp -d)
