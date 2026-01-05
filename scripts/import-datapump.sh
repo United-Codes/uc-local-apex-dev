8802d0d43d7a38d393a3bcccd9dbe0c948b184e2
phartenfeller <pharti@hotmail.de>
2026-01-05 12:11:18 +0100

    Enhance import-datapump script: update usage message and support optional remap schema parameter

--- scripts/import-datapump.sh
+++ scripts/import-datapump.sh
@@ -6,7 +6,7 @@ source ./scripts/util/load_env.sh
 
 # check parameter is passed
 if [ -z "$1" ]; then
-  echo "Usage: $0 <schema_name>"
+  echo "Usage: $0 <schema_name> [-rs <remap_schema_name>]"
   exit 1
 fi
 
@@ -15,22 +15,31 @@ USERNAME_UPPER=$(echo $USERNAME | tr '[:lower:]' '[:upper:]')
 USERNAME_LOWER=$(echo $USERNAME | tr '[:upper:]' '[:lower:]')
 SQLCRED_NAME="${DB_CONN_BASE}-${USERNAME_LOWER}"
 
+# Parse optional -rs parameter
+REMAP_SCHEMA=""
+SCHEMA_NAME="${USERNAME_LOWER}"
+if [ "$2" = "-rs" ] && [ -n "$3" ]; then
+  REMAP_SCHEMA_NAME=$(echo $3 | tr '[:upper:]' '[:lower:]')
+  REMAP_SCHEMA="-remapschemas ${REMAP_SCHEMA_NAME}=${USERNAME_LOWER}"
+  SCHEMA_NAME="${REMAP_SCHEMA_NAME}"
+fi
+
 sql -name $SQLCRED_NAME <<SQL
   select user from dual;
 
   datapump import -
-  -schemas ${USERNAME_LOWER} -
+  -schemas ${SCHEMA_NAME} -
   -directory datapump_import_dir -
   -dumpfile ${USERNAME_LOWER}.dmp -
   -logfile ${USERNAME_LOWER}.log -
-  -version latest
+  -version latest ${REMAP_SCHEMA}
 
   datapump import -
-  -schemas ${USERNAME_LOWER} -
+  -schemas ${SCHEMA_NAME} -
   -directory datapump_import_dir -
   -dumpfile ${USERNAME_LOWER}.dmp -
   -logfile ${USERNAME_LOWER}.log -
-  -version latest
+  -version latest ${REMAP_SCHEMA}
 
   commit;
 
